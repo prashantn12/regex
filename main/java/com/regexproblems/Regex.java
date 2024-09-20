e4f78ec4277d489e6e53889e5ab37a3a9c0699d3
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-20 12:51:46 +0530

    Added: To validate both formats of the PIN code — 400088 and 400 088

--- main/java/com/regexproblems/Regex.java
+++ main/java/com/regexproblems/Regex.java
@@ -6,21 +6,21 @@ import java.util.regex.Matcher;
 
 
 public class Regex {
+
     public static boolean validatePinCode(String pinCode) {
-        String regex = "^[1-9][0-9]{5}$";
+        String regex = "^[1-9][0-9]{2}\\s?[0-9]{3}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(pinCode);
         return matcher.matches();
     }
 
     public static void main(String[] args) {
-        String pinCode = "400088B";
+        String pinCode1 = "400088";
+        String pinCode2 = "400 088";
 
-        if (validatePinCode(pinCode)) {
-            System.out.println(pinCode + " is a valid PIN code.");
-        } else {
-            System.out.println(pinCode + " is not a valid PIN code.");
-        }
+        // Validate the PIN codes
+        System.out.println(pinCode1 + " is " + (validatePinCode(pinCode1) ? "valid" : "invalid") + " PIN code.");
+        System.out.println(pinCode2 + " is " + (validatePinCode(pinCode2) ? "valid" : "invalid") + " PIN code.");
     }
 
 }
