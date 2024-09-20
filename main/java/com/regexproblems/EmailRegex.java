b41232a4c005c6aea485fcb6c4e8668777dc3e9d
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-20 13:46:25 +0530

    Added:  make sure only following are valid special characters _,+, -,. proceeding to xyz

--- main/java/com/regexproblems/EmailRegex.java
+++ main/java/com/regexproblems/EmailRegex.java
@@ -6,23 +6,29 @@ public class EmailRegex {
 
 
     public static boolean validateEmail(String email) {
-        String regex = "^abc@bridgelabz\\.co.*$";
 
+        String regex = "^abc([._+\\-][a-zA-Z0-9]+)?@bridgelabz\\.co(\\.[a-z]{2,})?$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
+
+
     public static void main(String[] args) {
         String email1 = "abc@bridgelabz.co";
-        String email2 = "abc@bridgelabz.co.in";
-        String email3 = "abc@bridgelabz.com";
-        String email4 = "abc@xyz.co";
+        String email2 = "abc.xyz@bridgelabz.co.in";
+        String email3 = "abc+xyz@bridgelabz.co";
+        String email4 = "abc_xyz@bridgelabz.co";
+        String email5 = "abc-xyz@bridgelabz.co.in";
+        String email6 = "abc@xyz.co";  // Invalid
 
-        // Validate the email addresses
+        
         System.out.println(email1 + " is " + (validateEmail(email1) ? "valid" : "invalid") + " email.");
         System.out.println(email2 + " is " + (validateEmail(email2) ? "valid" : "invalid") + " email.");
         System.out.println(email3 + " is " + (validateEmail(email3) ? "valid" : "invalid") + " email.");
         System.out.println(email4 + " is " + (validateEmail(email4) ? "valid" : "invalid") + " email.");
+        System.out.println(email5 + " is " + (validateEmail(email5) ? "valid" : "invalid") + " email.");
+        System.out.println(email6 + " is " + (validateEmail(email6) ? "valid" : "invalid") + " email.");
     }
 }
 
