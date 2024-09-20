8d32d9f9f8315125e97e277b4a2aea5fbaa73535
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-20 13:59:28 +0530

    Added: we will finalize the regex expression to support optional country code top-level domains (TLDs)

--- main/java/com/regexproblems/EmailRegex.java
+++ main/java/com/regexproblems/EmailRegex.java
@@ -5,9 +5,9 @@ import java.util.regex.Matcher;
 public class EmailRegex {
 
 
-    public static boolean validateEmail(String email) {
 
-        String regex = "^abc([._+\\-][a-zA-Z0-9]+)?@bridgelabz\\.co(\\.[a-z]{2,})?$";
+    public static boolean validateEmail(String email) {
+        String regex = "^abc([._+\\-][a-zA-Z0-9]+)?@bridgelabz\\.co(\\.[a-z]{2})?$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
@@ -21,14 +21,18 @@ public class EmailRegex {
         String email4 = "abc_xyz@bridgelabz.co";
         String email5 = "abc-xyz@bridgelabz.co.in";
         String email6 = "abc@xyz.co";  // Invalid
+        String email7 = "abc@bridgelabz.co.us"; // Invalid (more than 2 characters)
+        String email8 = "abc@bridgelabz.co.in"; // Valid
 
-        
+        // Validate the email addresses
         System.out.println(email1 + " is " + (validateEmail(email1) ? "valid" : "invalid") + " email.");
         System.out.println(email2 + " is " + (validateEmail(email2) ? "valid" : "invalid") + " email.");
         System.out.println(email3 + " is " + (validateEmail(email3) ? "valid" : "invalid") + " email.");
         System.out.println(email4 + " is " + (validateEmail(email4) ? "valid" : "invalid") + " email.");
         System.out.println(email5 + " is " + (validateEmail(email5) ? "valid" : "invalid") + " email.");
         System.out.println(email6 + " is " + (validateEmail(email6) ? "valid" : "invalid") + " email.");
+        System.out.println(email7 + " is " + (validateEmail(email7) ? "valid" : "invalid") + " email.");
+        System.out.println(email8 + " is " + (validateEmail(email8) ? "valid" : "invalid") + " email.");
     }
 }
 
