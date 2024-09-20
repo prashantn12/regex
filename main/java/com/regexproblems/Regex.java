3ba25b73a339764886d394fc9461d645318f8ec0
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-20 12:00:46 +0530

    Added: Welcome to regex problems

---
+++ main/java/com/regexproblems/Regex.java
@@ -0,0 +1,8 @@
+package com.regexproblems;
+import java.util.*;
+
+public class Regex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to regex problem");
+    }
+}
